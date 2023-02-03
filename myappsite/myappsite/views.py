27a7a3a5be4378334b50894f0d7726359f8bb275
2200159542 <2200159542@qq.com>
2023-02-03 13:32:13 +0800

    remove test project

--- myappsite/myappsite/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-def index(request):
-    if request.method == 'GET':
-        return HttpResponse('hgkgujl')
-        #return render(request,'/index/index.html')
--- myappsite/userm/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-
-    path('reg/', views.reg2),
-    path('log/', views.login)
-]
